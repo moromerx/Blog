ea68c28f47f2b2429cfa3952843d41d4cccbe64c
moromerx <162036545+moromerx@users.noreply.github.com>
2024-05-05 01:47:02 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "MoRoMeR",
-    image: "/profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "IT Security Devotee",
     bio: "Blog",
     email: "amaanzmail@gmail.com",
